48831fd11071b0d8bb9daf4729b7272552d4cfc4
Steeves Obiang Ndam <steevesobiang@gmail.com>
2015-03-30 16:10:27 +0200

    affichage des voyages d'un utilisateur dans une listView depuis la Bdd

--- app/src/main/java/com/obisteeves/meetuworld/Tabs/TabTravel.java
+++ app/src/main/java/com/obisteeves/meetuworld/Tabs/TabTravel.java
@@ -14,23 +14,35 @@ import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+
 import com.obisteeves.meetuworld.PageAndroid.addTravel;
 import com.obisteeves.meetuworld.R;
+import com.obisteeves.meetuworld.Utils.NetworkRequestAdapter;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Observable;
+import java.util.Observer;
 
 import static com.obisteeves.meetuworld.Utils.Utilities.dialogPerso;
 
 /**
  * Created by hp1 on 21-01-2015.
  */
-public class TabTravel extends Fragment {
+public class TabTravel extends Fragment implements Observer{
 
     ImageButton FAB;
+    private HashMap<String,String> listViewMap = new HashMap<String, String>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_tab_travel, container, false);
         ajouterVoyage(v);
-        listViewVoyage(v);
+        listViewVoyage();
         return v;
     }
 
@@ -47,7 +59,7 @@ public class TabTravel extends Fragment {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getActivity(),addTravel.class);
+                Intent intent = new Intent(getActivity(), addTravel.class);
                 startActivity(intent);
             }
         });
@@ -56,29 +68,59 @@ public class TabTravel extends Fragment {
 
     /**
      * listViewVoyage qui permet d'afficher les voyages de l'utilisateur dans une ListView
-     * @param v
-     * @type View
+     *
      */
 
-    public  void listViewVoyage(View v){
+    public  void listViewVoyage(){
 
-        String[] voyages={"Gabon","Cameroun","Guinée-équatorial","Espagne"};
-
-        ListAdapter  voyagesAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_expandable_list_item_1,voyages);
-        ListView voyagesListView = (ListView) v.findViewById(R.id.voyageListView);
-        voyagesListView.setAdapter(voyagesAdapter);
-
-        voyagesListView.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        dialogPerso("info sur le voyage","Information","retour",getActivity());
-                    }
-                }
-        );
-
+        NetworkRequestAdapter net = new NetworkRequestAdapter(this.getActivity());
+        net.addObserver(this);
+        String address = getResources().getString(R.string.serveurAdd)
+                + getResources().getString(R.string.listVoyageUser);
+        net.setUrl(address);
+        net.send();
     }
 
 
+    @Override
+    public void update(Observable observable, Object data) {
+
+        NetworkRequestAdapter resultat = ((NetworkRequestAdapter) observable);
+        String netReq = String.valueOf(NetworkRequestAdapter.OK);
+        if (data.toString().equals(netReq))
+        {
+            try
+            {
+                JSONArray voyages =  resultat.getResult().getJSONArray("voyages");
+                String[] listvoyages = new String[voyages.length()];
+                for (int i = 0; i < voyages.length(); i++)
+                {
+                    JSONObject json = voyages.getJSONObject(i);
+                    String pays = json.getString("pays");
+                    String id = json.getString("id");
+                    listViewMap.put(pays,id);
+                    listvoyages[i] = pays;
+                }
+
+                ListAdapter  voyagesAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_expandable_list_item_1,listvoyages);
+                ListView voyagesListView = (ListView) getActivity().findViewById(R.id.voyageListView);
+                voyagesListView.setAdapter(voyagesAdapter);
+                voyagesListView.setOnItemClickListener(
+                        new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                dialogPerso("info sur le voyage","Information","retour",getActivity());
+                            }
+                        }
+                );
+
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
 
