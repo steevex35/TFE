4ad9113365a4cb99ea96152000debfe2e85fc709
Steeves Obiang Ndam <steevesobiang@gmail.com>
2015-05-24 11:51:54 +0200

    test avatar

--- app/src/main/java/com/obisteeves/meetuworld/Tabs/TabProfil.java
+++ app/src/main/java/com/obisteeves/meetuworld/Tabs/TabProfil.java
@@ -1,13 +1,18 @@
 package com.obisteeves.meetuworld.Tabs;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.obisteeves.meetuworld.PageAndroid.modifierProfil;
@@ -16,6 +21,10 @@ import com.obisteeves.meetuworld.Utils.NetworkRequestAdapter;
 
 import org.json.JSONException;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -23,6 +32,12 @@ import java.util.Observer;
  * Created by hp1 on 21-01-2015.
  */
 public class TabProfil extends Fragment implements Observer {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView avatar;
+    private ImageButton boutonAlter;
+    private String mCurrentPhotoPath;
+    private File photoFile = null;
+    private File image;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,7 +45,8 @@ public class TabProfil extends Fragment implements Observer {
 
         afficheProfil(v);
 
-        ImageButton boutonAlter = (ImageButton) v.findViewById(R.id.modifier_profil);
+        avatar = (ImageView) v.findViewById(R.id.avatar);
+        boutonAlter = (ImageButton) v.findViewById(R.id.modifier_profil);
         boutonAlter.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -41,9 +57,71 @@ public class TabProfil extends Fragment implements Observer {
             }
         });
 
+        avatar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                takePicture();
+
+            }
+        });
+
         return v;
 
     }
+
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        if (v.getId() == R.id.avatar) {
+
+            getActivity().getMenuInflater().inflate(R.menu.menu_avatar, menu);
+
+        }
+
+    }
+
+    private void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK) {
+            Bundle extras = data.getExtras();
+
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            avatar.setImageBitmap(imageBitmap);
+        }
+    }
+
 
     public void afficheProfil(View v) {
         NetworkRequestAdapter net = new NetworkRequestAdapter(this.getActivity());
